e531bf46153180cda20ba389895703ae034ba20f
kozyilmaz <kazim@monolytic.com>
2018-01-04 20:32:01 +0000

    up

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -27,35 +27,8 @@ cat /proc/meminfo
 echo "\n=========================="
 echo "NVIDIA Jetson TX2 Throttle"
 echo "==========================\n"
-
-echo "Turn on fan for safety"
-echo 255 > /sys/kernel/debug/tegra_fan/target_pwm
-echo "Fan setting"
-cat /sys/kernel/debug/tegra_fan/target_pwm
-echo "Cores active"
-cat /sys/devices/system/cpu/online
-echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo performance > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-echo performance > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-echo performance > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor
-echo "Scaling governors (0, 3, 4, 5)"
-cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-cat /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-cat /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor
-echo "CPU available frequencies"
-cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies
-cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
-cat /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
-cat /sys/devices/system/cpu/cpu5/cpufreq/scaling_max_freq > /sys/devices/system/cpu/cpu5/cpufreq/scaling_min_freq
-echo "CPU minimum cycle frequencies (0, 3, 4, 5)"
-cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
-cat /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
-cat /sys/devices/system/cpu/cpu5/cpufreq/scaling_min_freq
-echo "Max Performance Settings Done"
-
+echo "$ sudo periodictaskreleaser/Benchmark/TX-max_perf.sh"
+sudo periodictaskreleaser/Benchmark/TX-max_perf.sh
 
 echo "\n=============================="
 echo "Caffe Deep Learning Benchmarks"
@@ -74,10 +47,14 @@ echo "================================\n"
 mkdir -p $BENCHMARK_FOLDER/periodictaskreleaser
 echo "$ cd periodictaskreleaser/Benchmark"
 cd periodictaskreleaser/Benchmark
+echo "$ sudo ./TX-max_perf.sh"
+sudo ./TX-max_perf.sh
 echo "$ ruby run_benchmarks.rb"
-ruby run_benchmarks.rb
+#ruby run_benchmarks.rb
 echo "$ cp output.json ../../$BENCHMARK_FOLDER/periodictaskreleaser/"
 cp output.json ../../$BENCHMARK_FOLDER/periodictaskreleaser/
+echo "$ tar -czvf results.tar.gz results/"
+tar -czvf results.tar.gz results/
 echo "$ cp results.tar.gz ../../$BENCHMARK_FOLDER/periodictaskreleaser/"
 cp results.tar.gz ../../$BENCHMARK_FOLDER/periodictaskreleaser/
 echo "$ cp generate_plots.py ../../$BENCHMARK_FOLDER/periodictaskreleaser/"
